e7e6029129600a17ddcc4e75c8124611ae856b15
jimmy-yu-work <jimmy.yu@viitorsemi.com>
2026-06-26 09:43:44 +0800

    feat: add RelationKind and RelationMetadata to Domain

---
+++ src/Struo.Domain/Metadata/Enums/RelationKind.cs
@@ -0,0 +1,3 @@
+namespace Struo.Domain.Metadata.Enums;
+
+public enum RelationKind { ManyToOne, OneToMany, ManyToMany }
---
+++ src/Struo.Domain/Metadata/Models/RelationMetadata.cs
@@ -0,0 +1,18 @@
+using Struo.Domain.Metadata.Enums;
+
+namespace Struo.Domain.Metadata.Models;
+
+public sealed record RelationMetadata
+{
+    public required string Name { get; init; }
+    public required string Label { get; init; }
+    public required RelationKind Kind { get; init; }
+    public required string TargetCollection { get; init; }
+    public required RelationInterface Interface { get; init; }
+    public string? ForeignKey { get; init; }
+    public string? DisplayTemplate { get; init; }
+    public string? PickerQuery { get; init; }
+    public OnDelete OnDelete { get; init; }
+    public bool Editable { get; init; }
+    public bool SelfReferencing { get; init; }
+}
