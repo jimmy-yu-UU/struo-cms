ea145676f6c377a573f1a9bfe573b3949d41c02f
Jimmy Yu <13616848+jimmy-yu-UU@users.noreply.github.com>
2026-07-06 14:15:49 +0800

    feat(sanitizer): allow basic tables, sub/sup, and color/text-align styles (7g)

--- tests/Struo.Tests/Security/GanssHtmlSanitizerTests.cs
+++ tests/Struo.Tests/Security/GanssHtmlSanitizerTests.cs
@@ -29,10 +29,10 @@ public class GanssHtmlSanitizerTests
     }
 
     [Fact]
-    public void Strips_iframe_and_inline_style()
+    public void Strips_iframe()
     {
-        var clean = _s.Sanitize("<iframe src=\"http://evil\"></iframe><p style=\"color:red\">t</p>");
-        clean.Should().NotContain("iframe").And.NotContain("style=");
+        var clean = _s.Sanitize("<iframe src=\"http://evil\"></iframe><p>t</p>");
+        clean.Should().NotContain("iframe");
         clean.Should().Contain("t");
     }
 
@@ -63,5 +63,65 @@ public class GanssHtmlSanitizerTests
     {
         var clean = _s.Sanitize("<a href=\"https://ok\">l</a>");
         clean.Should().Contain("rel=").And.Contain("noopener");
+    }
+
+    [Fact]
+    public void Keeps_basic_table_markup()
+    {
+        var html = "<table><tbody><tr><th><p>H</p></th></tr>"
+                 + "<tr><td><p>c</p></td></tr></tbody></table>";
+        var clean = _s.Sanitize(html);
+        clean.Should().Contain("<table>").And.Contain("<tbody>").And.Contain("<tr>")
+             .And.Contain("<th>").And.Contain("<td>");
+    }
+
+    [Fact]
+    public void Strips_colspan_rowspan_and_colwidth_from_table_cells()
+    {
+        var clean = _s.Sanitize(
+            "<table><tbody><tr><td colspan=\"2\" rowspan=\"3\" colwidth=\"120\"><p>c</p></td></tr></tbody></table>");
+        clean.Should().NotContain("colspan").And.NotContain("rowspan").And.NotContain("colwidth");
+        clean.Should().Contain("<td>");
+    }
+
+    [Fact]
+    public void Keeps_text_align_style_on_paragraphs_and_headings()
+    {
+        var clean = _s.Sanitize(
+            "<p style=\"text-align: center\">t</p><h2 style=\"text-align: right\">h</h2>");
+        clean.Should().Contain("text-align").And.Contain("center").And.Contain("right");
+    }
+
+    [Fact]
+    public void Keeps_color_style_on_span()
+    {
+        var clean = _s.Sanitize("<p><span style=\"color: #e11d48\">red</span></p>");
+        clean.Should().Contain("<span").And.Contain("color");
+        clean.Should().Contain("red");
+    }
+
+    [Fact]
+    public void Strips_non_allowlisted_css_properties_but_keeps_allowed_ones()
+    {
+        var clean = _s.Sanitize(
+            "<p style=\"text-align: center; position: fixed; font-size: 99px\">t</p>");
+        clean.Should().Contain("text-align").And.NotContain("position").And.NotContain("font-size");
+    }
+
+    [Fact]
+    public void Strips_dangerous_css_values()
+    {
+        var clean = _s.Sanitize(
+            "<p style=\"background: url(//evil/x.png)\">a</p>"
+          + "<p style=\"width: expression(alert(1))\">b</p>");
+        clean.Should().NotContain("url(").And.NotContain("expression");
+        clean.Should().Contain("a").And.Contain("b");
+    }
+
+    [Fact]
+    public void Keeps_sub_and_sup()
+    {
+        var clean = _s.Sanitize("<p>H<sub>2</sub>O and x<sup>2</sup></p>");
+        clean.Should().Contain("<sub>").And.Contain("<sup>");
     }
 }
