3353bceeb850636000d5e378a29f60709e0d0a6c
Jimmy Yu <13616848+jimmy-yu-UU@users.noreply.github.com>
2026-07-07 15:53:56 +0800

    feat: fail-fast on Translatable multi-value/KeyValue/Files fields at scan (review M4)

--- tests/Struo.Tests/Metadata/MetadataScannerTests.cs
+++ tests/Struo.Tests/Metadata/MetadataScannerTests.cs
@@ -205,4 +205,40 @@ public class MetadataScannerTests
         meta.Sortable.Should().BeFalse();
         meta.Searchable.Should().BeFalse();
     }
+
+    [SugarTable("bad_translatable_files")]
+    [CmsCollection("BadTranslatableFiles")]
+    private sealed class BadTranslatableFiles : AuditableEntity
+    {
+        [SugarColumn(IsPrimaryKey = true)] public override Guid Id { get; set; }
+
+        [CmsField(Label = "Gallery", Interface = FieldInterface.Files, Translatable = true)]
+        public List<Guid> Gallery { get; set; } = new();
+    }
+
+    [SugarTable("ok_translatable_json")]
+    [CmsCollection("OkTranslatableJson")]
+    private sealed class OkTranslatableJson : AuditableEntity
+    {
+        [SugarColumn(IsPrimaryKey = true)] public override Guid Id { get; set; }
+
+        // Json is intentionally NOT caught by the guard (a string that could be translatable later).
+        [CmsField(Label = "Attributes", Interface = FieldInterface.Json, Translatable = true)]
+        public string? Attributes { get; set; }
+    }
+
+    [Fact]
+    public void Translatable_files_field_fails_fast()
+    {
+        var act = () => MetadataScanner.ScanTypes([typeof(BadTranslatableFiles)]);
+        act.Should().Throw<MetadataException>()
+            .WithMessage("*Gallery*cannot be translatable*");
+    }
+
+    [Fact]
+    public void Translatable_json_field_is_allowed()
+    {
+        var act = () => MetadataScanner.ScanTypes([typeof(OkTranslatableJson)]);
+        act.Should().NotThrow();
+    }
 }
