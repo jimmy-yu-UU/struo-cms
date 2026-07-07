1cf30c6045ac73dd1670b5ba001a366bf68fe932
jimmy-yu-work <jimmy.yu@viitorsemi.com>
2026-07-07 09:39:14 +0800

    feat: [CmsOptions] label is optional (bare value => label defaults to value)

--- src/Struo.Domain/Metadata/Attributes/CmsOptionsAttribute.cs
+++ src/Struo.Domain/Metadata/Attributes/CmsOptionsAttribute.cs
@@ -3,6 +3,6 @@ namespace Struo.Domain.Metadata.Attributes;
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
 public sealed class CmsOptionsAttribute(params string[] options) : Attribute
 {
-    // Each entry is "value:label".
+    // Each entry is "value:label", or bare "value" (label defaults to the value).
     public IReadOnlyList<string> Options { get; } = options;
 }
